f4c26eb8bda59ccf394506f37adf76f9984267b8
Sonu-Hansda <sonukumarhansda61@gmail.com>
2024-01-09 11:28:17 +0530

    fix for v1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,26 +3,38 @@ import { HiOutlineHeart } from "react-icons/hi2";
 import {RxReload} from "react-icons/rx";
 import { Link } from "react-router-dom";
 import Product from "../interface/Product";
+import errorpage from "../images/500.png";
 
 function Home() {
     const [products,setProducts] = useState<Product[]>([]);
     const [error ,setError] = useState<Boolean>(false);
     useEffect(()=>{
-      fetch("https://fakestoreapi.com/products")
-      .then((res)=>{
-        return res.json();
-      })
-      .then((data:Product[])=>{
-        setProducts(data);
-        setError(data.length > 0 ? false : true);
-      });
+      try{
+
+        fetch("https://fakestoreapi.com/products")
+        .then((res)=>{
+          return res.json();
+        })
+        .then((data:Product[])=>{
+          setProducts(data);
+          
+          setError(data.length > 0 ? false : true);
+          
+        });
+      }catch{
+        setError(true);
+      }
     },[]);
-  return (
-    <section className="mx-12 py-4 grid grid-cols-1 md:grid-cols-4 gap-4">
+    if (error){
+        <img src={errorpage} alt="500 :  internal server erorr" />
+    } else{
+
+      return (
+        <section className="mx-12 py-4 grid grid-cols-1 md:grid-cols-4 gap-4">
         {products.length > 0 ?
          <>
          
-         {products.map((product, index) => {
+         {products.map((product) => {
              return (
                  <div className="flex flex-col justify-between p-12 bg-white transition-all duration-300 ease-linear hover:shadow-xl border">
                  <span className="text-xl flex justify-end hover:text-2xl transition-all duration-300 ease-linear cursor-pointer">
@@ -36,13 +48,14 @@ function Home() {
                 <button className="w-full rounded-md hover:shadow-xl hover:translate-y-2 transition-all duration-300 delay-150 bg-black py-2 text-white">Buy now</button>
                </div>
              );
-           })}
+            })}
          </> : 
          <div className="flex justify-center">
             <RxReload/>
          </div> }
       </section>
   )
+}
 }
 
 export default Home
